1338288b2f484f425cbc1dda700dc477153269bd
Promisekel <101825563+Promisekel@users.noreply.github.com>
2024-12-16 03:23:43 +0000

    Update resume.py

--- resume.py
+++ resume.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def main():
-
+    # Page configuration
     st.set_page_config(page_title="Promise Bansah - Bio", page_icon="📄", layout="wide")
 
     # Sidebar Navigation
@@ -9,7 +9,10 @@ def main():
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIBfJxFgGX2d961bTaupSiOuAS8TmF_7BC0g&s",
         width=150,
     )
-    menu = ["About Me", "Key Projects", "Education", "Current Focus", "Contact"]
+    menu = ["About Me", "Key Projects", "Education", "Skills & Competencies", "Current Focus", 
+            "Training Workshops and Conferences", "Membership, License and Certification", 
+            "Achievements", "Reviewed Manuscripts for Potential Publication", 
+            "Community Involvement", "Interests/Hobbies", "Contact"]
     choice = st.sidebar.radio("Go to", menu)
 
     # Styling
@@ -54,7 +57,7 @@ def main():
             - **Infectious Disease Epidemiology**  
             - **Clinical Research**  
             - **Publication**  
-            - **Data Analysis and Presentation**
+            - **Data Analysis and Presentation** 
 
             ### PROGRAMMING
             - **Software Engineering**  
@@ -69,85 +72,213 @@ def main():
             - **Digital Health**
             """
         )
+    elif choice == "Reviewed Manuscripts for Potential Publication":
+        st.header("Reviewed Manuscripts for Potential Publication")
+        st.markdown(
+            """
+            **Manuscripts Currently Under Review:**
+            - **Factors contributing to paracetamol overdoses (Intentional and Accidental) among the population in the United Kingdom: A Systematic Review Protocol** for BMJ Open  
+              (Manuscript ID: bmjopen-2024-090135)
+            - **Exploring family-based immigrant youth substance use prevention programs: a scoping review** for Substance Abuse: Research and Treatment  
+              (Manuscript ID: SAT-23-0046)
+            - **Correlates of Staff Acceptability of a Novel Telemedicine Buprenorphine Program in a Rural Detention Centre**  
+              (Manuscript ID: SAT-23-0016)
+            """
+        )
     elif choice == "Key Projects":
         st.header("Key Projects")
         st.markdown(
-        """
-        <style>
-        .key-project {
-            background-color: #f8f9fa;
-            padding: 15px;
-            margin: 10px 0;
-            border-radius: 10px;
-            box-shadow: 0px 2px 5px rgba(0,0,0,0.1);
-        }
-        .project-title {
-            font-weight: bold;
-            font-size: 18px;
-            color: #343a40;
-        }
-        .project-link {
-            color: #007bff;
-            text-decoration: none;
-            font-size: 16px;
-        }
-        .project-link:hover {
-            text-decoration: underline;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
-        projects = [
-        {
-            "title": "Developed deep learning model for predicting pneumonia using X-ray images",
-            "link": "https://github.com/Promisekel/pneumonia-detection",
-        },
-        {
-            "title": "Developed Android app for File Management",
-            "link": "https://play.google.com/store/apps/details?id=com.example.filemanager",
-        },
-        {
-            "title": "Developed Android app for project management at KCCR",
-            "link": "",
-        },
-        {
-            "title": "Developed a Bible Verse Android App",
-            "link": "https://play.google.com/store/apps/details?id=com.example.bibleverse",
-        },
-        {
-            "title": "CEO of Premiere Scientific and Research Publication Club",
-            "link": "https://premirescientific.com",
-        },
-    ]
-    
-
-    for project in projects:
-        st.markdown(
-            f"""
-            <div class="key-project">
-                <div class="project-title">{project['title']}</div>
-                {'<a class="project-link" href="' + project['link'] + '" target="_blank">View Project</a>' if project['link'] else '<span style="color: #6c757d;">Link unavailable</span>'}
-            </div>
+            """
+            <style>
+            .key-project {
+                background-color: #f8f9fa;
+                padding: 15px;
+                margin: 10px 0;
+                border-radius: 10px;
+                box-shadow: 0px 2px 5px rgba(0,0,0,0.1);
+            }
+            .project-title {
+                font-weight: bold;
+                font-size: 18px;
+                color: #343a40;
+            }
+            .project-link {
+                color: #007bff;
+                text-decoration: none;
+                font-size: 16px;
+            }
+            .project-link:hover {
+                text-decoration: underline;
+            }
+            </style>
             """,
             unsafe_allow_html=True,
         )
-    if choice == "Education":
+        projects = [
+            {
+                "title": "Developed deep learning model for predicting pneumonia using X-ray images",
+                "link": "https://github.com/Promisekel/pneumonia-detection",
+            },
+            {
+                "title": "Developed Android app for File Management",
+                "link": "https://play.google.com/store/apps/details?id=com.example.filemanager",
+            },
+            {
+                "title": "Developed Android app for project management at KCCR",
+                "link": "",
+            },
+            {
+                "title": "Developed a Bible Verse Android App",
+                "link": "https://play.google.com/store/apps/details?id=com.example.bibleverse",
+            },
+            {
+                "title": "CEO of Premiere Scientific and Research Publication Club",
+                "link": "https://premirescientific.com",
+            },
+        ]
+
+        for project in projects:
+            st.markdown(
+                f"""
+                <div class="key-project">
+                    <div class="project-title">{project['title']}</div>
+                    {'<a class="project-link" href="' + project['link'] + '" target="_blank">View Project</a>' if project['link'] else '<span style="color: #6c757d;">Link unavailable</span>'}
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
+    elif choice == "Community Involvement":
+        st.header("Community Involvement")
+        st.markdown(
+            """
+            **Organized Data Analysis and Research Method Workshops for Undergraduate Students**  
+            I have been actively involved in organizing workshops aimed at improving data analysis and research methods skills among undergraduate students. The goal of these workshops is to equip students with the skills they need to excel in their academic pursuits and contribute to their research projects. These sessions cover basic data analysis, research methodologies, and the importance of evidence-based approaches in research.
+            """
+        )
+    elif choice == "Interests/Hobbies":
+        st.header("Interests/Hobbies")
+        st.markdown(
+            """
+            **Programming**  
+            I enjoy programming and building applications to solve real-world problems, particularly in health and public health contexts.
+
+            **Research**  
+            My passion for research drives me to continuously explore new methodologies and technologies in the fields of data science, public health, and epidemiology.
+
+            **Gaming**  
+            In my free time, I indulge in gaming, which provides a fun and interactive way to unwind and sharpen my problem-solving skills.
+            """
+        )
+    elif choice == "Training Workshops and Conferences":
+        st.header("Training Workshops and Conferences")
+        st.markdown(
+            """
+            **Workshops and Certifications:**
+            - **Next Generation Sequencing (NGS) and Bioinformatics Workshop** on microbial pathogens | KCCR  
+            - **Introduction to Biomedical and Health Research (B-HERE) Workshop** | KCCR  
+            - **Building Self-Service Analytics with Snowflake Cortex Analyst** | Snowflake  
+            - **5-Day Gen AI Intensive Course** | Kaggle/Google  
+            - **GCP and GLCP Training** | FDA  
+            - **Qualitative and Quantitative Data Analysis** | UHAS  
+            - **Spatial Data Analysis using ArcMap GIS** | UHAS  
+            - **REDCap Software Training** | UHAS  
+            """
+        )
+    elif choice == "Membership, License and Certification":
+        st.header("Membership, License and Certification")
+        st.markdown(
+            """
+            **Memberships and Certifications:**
+            - **Certified Data Analyst** | Data Science Academy  
+            - **AI and Machine Learning Certification** | Coursera  
+            - **Member of Global Health Research Network**  
+            - **Member of Public Health Data Science Network**  
+            - **Certified Health Informatics Professional (CHIP)** | Health Informatics Certification Organization  
+            """
+        )
+    elif choice == "Achievements":
+        st.header("Achievements")
+        st.markdown(
+            """
+            **Notable Achievements:**
+            - Developed **deep learning model for predicting pneumonia** using X-ray images  
+              [View Model](https://github.com/Promisekel/pneumonia-detection)
+            - Developed **Android app for File Management** on Google Play  
+              [View App](https://play.google.com/store/apps/details?id=com.example.filemanager)
+            - Developed **Android app for project management at KCCR**
+            - Developed **Bible Verse Android App** on Google Play  
+              [View App](https://play.google.com/store/apps/details?id=com.example.bibleverse)
+            - CEO of **Premiere Scientific and Research Publication Club**  
+              [View Club](https://premirescientific.com)
+            """
+        )
+    elif choice == "Education":
         st.header("Education")
         st.markdown(
             """
-            - **Bachelors in Public Health, Disease Control** - University of Health and Allied Sciences, Ghana (2019)  
-            - **Advanced Data Science & Analytics Certification** - Koforidua Technical University, Ghana (2024)  
-            - **MPhil in Biomedical Data Science** (In Progress)
-            """
+            <style>
+            .education-entry {
+                background-color: #f8f9fa;
+                padding: 15px;
+                margin: 10px 0;
+                border-radius: 10px;
+                box-shadow: 0px 2px 5px rgba(0,0,0,0.1);
+            }
+            .education-title {
+                font-weight: bold;
+                font-size: 18px;
+                color: #343a40;
+            }
+            .education-institution {
+                color: #6c757d;
+                font-size: 16px;
+            }
+            .education-date {
+                color: #007bff;
+                font-size: 14px;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True,
         )
+        education = [
+            {
+                "title": "Advanced Data Analytics",
+                "institution": "Koforidua Technical University, Ghana",
+                "date": "2024"
+            },
+            {
+                "title": "Machine Learning and AI",
+                "institution": "Koforidua Technical University, Ghana",
+                "date": "2024"
+            },
+            {
+                "title": "Bachelor of Science in Public Health (Disease Control)",
+                "institution": "University of Health and Allied Sciences, Ghana",
+                "date": "2019"
+            }
+        ]
+
+        for entry in education:
+            st.markdown(
+                f"""
+                <div class="education-entry">
+                    <div class="education-title">{entry['title']}</div>
+                    <div class="education-institution">{entry['institution']}</div>
+                    <div class="education-date">{entry['date']}</div>
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
     elif choice == "Contact":
         st.header("Contact")
         st.markdown(
             """
-            - **Email:** promise.bansah@example.com  
-            - **GitHub:** [Promisekel](https://github.com/Promisekel)  
-            - **LinkedIn:** [Promise Bansah](https://www.linkedin.com/in/promisekel/)  
+            You can reach me via:
+            - Email: **promisekel@healthscience.org**
+            - Phone: **+233 123 456 789**
+            - LinkedIn: **[Promise Bansah](https://www.linkedin.com/in/promisekel)**
+            - GitHub: **[PromiseBansah](https://github.com/Promisekel)**
             """
         )
 
